15dbef5c7a74f27f425015bbf4820cca601f3c8d
Felicity <felicitymuhonja33@gmail.com>
2024-03-20 14:58:30 +0300

    feat:add phase-1-arithmetic-lab

--- index.js
+++ index.js
@@ -1,2 +1,30 @@
-
 // Write your code here
+// Task 1
+const num1 = 31;
+const num2 = 2;
+const multiply = num1 * num2;
+// multiply is set to an equation that miltiplies num1 and num2
+console.log(multiply);
+//Output:20
+
+//Task 2
+const random = Math.ceil(Math.random() * 100);
+// generates a random integer greater than 0
+console.log(random);
+// Output:(random number greater than 0)
+
+// Task 3
+const num3 = 24;
+const num4 = 5;
+const mod = num3 % num4;
+//mod is set to an equation that calculates the remainder of num3 divided by num4
+console.log(mod);
+// Output: 4
+
+//Task 4
+const numbers = [5,10,15,20];
+// array of numbers
+const max = Math.max(...numbers);
+//finds the highest number on the set
+console.log(max);
+// Output:20
